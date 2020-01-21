8233169b7fafddd92f24f32f09fc93cc599c08bb
Joseph Yang <before30@gmail.com>
2020-01-22 00:16:02 +0900

    Change modulename

---
+++ metricex-demo-application/src/main/java/cc/before30/metricex/demo/DemoApplication.java
@@ -0,0 +1,18 @@
+package cc.before30.metricex.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * DemoApplication
+ *
+ * @author before30
+ * @since 2020/01/22
+ */
+
+@SpringBootApplication
+public class DemoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+}
---
+++ metricex-grpc-backend/src/main/java/cc/before30/metricex/backend/name/NameApplication.java
@@ -0,0 +1,17 @@
+package cc.before30.metricex.backend.name;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * NameApplication
+ *
+ * @author before30
+ * @since 2020/01/22
+ */
+@SpringBootApplication
+public class NameApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(NameApplication.class, args);
+    }
+}
