1c1264e2aee2b4b9060e8c679fc1e3e1e9a9d357
Joseph Yang <before30@gmail.com>
2020-02-26 11:51:56 +0900

    Add baseline

--- metricex-http-backend/src/main/resources/data.sql
+++
@@ -1,7 +0,0 @@
-insert into ages(age) values(10);
-insert into ages(age) values(21);
-insert into ages(age) values(32);
-insert into ages(age) values(43);
-insert into ages(age) values(54);
-insert into ages(age) values(65);
-insert into ages(age) values(76);
