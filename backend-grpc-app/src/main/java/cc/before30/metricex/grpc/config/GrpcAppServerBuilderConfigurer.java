b6862cf0a3398030a797c889bba469154c3b9c16
Joseph Yang <before30@gmail.com>
2020-02-28 20:22:31 +0900

    Change prometheus micrometer library

--- backend-grpc-app/src/main/java/cc/before30/metricex/grpc/config/GrpcAppServerBuilderConfigurer.java
+++ backend-grpc-app/src/main/java/cc/before30/metricex/grpc/config/GrpcAppServerBuilderConfigurer.java
@@ -1,11 +1,11 @@
 package cc.before30.metricex.grpc.config;
 
-import com.be_hase.grpc.micrometer.GrpcMetricsConfigure;
-import com.be_hase.grpc.micrometer.MicrometerServerInterceptor;
 import io.grpc.ServerBuilder;
 import io.grpc.protobuf.services.ProtoReflectionService;
-import io.micrometer.core.instrument.Metrics;
+import io.prometheus.client.CollectorRegistry;
 import lombok.NonNull;
+import me.dinowernli.grpc.prometheus.Configuration;
+import me.dinowernli.grpc.prometheus.MonitoringServerInterceptor;
 import org.lognet.springboot.grpc.GRpcServerBuilderConfigurer;
 import org.springframework.stereotype.Component;
 
@@ -21,23 +21,32 @@ import java.util.concurrent.Executor;
 @Component
 public class GrpcAppServerBuilderConfigurer extends GRpcServerBuilderConfigurer {
 
+    private final CollectorRegistry registry;
     private final Executor executor;
 
-    public GrpcAppServerBuilderConfigurer(@NonNull Executor executor) {
+    public GrpcAppServerBuilderConfigurer(@NonNull CollectorRegistry registry,
+                                          @NonNull Executor executor) {
+        this.registry = registry;
         this.executor = executor;
     }
 
     @Override
     public void configure(ServerBuilder<?> serverBuilder) {
-        GrpcMetricsConfigure configure =
-                GrpcMetricsConfigure.create()
-                    .withLatencyTimerConfigure(it -> {
-                        it.publishPercentiles(0.5, 0.9, 0.95, 0.99);
-                    });
+//        GrpcMetricsConfigure configure =
+//                GrpcMetricsConfigure.create()
+//                    .withLatencyTimerConfigure(it -> {
+//                        it.publishPercentiles(0.5, 0.9, 0.95, 0.99);
+//                    });
+        MonitoringServerInterceptor interceptor =
+                MonitoringServerInterceptor
+                .create(Configuration
+                        .cheapMetricsOnly()
+                        .withCollectorRegistry(registry));
 
         serverBuilder
                 .addService(ProtoReflectionService.newInstance())
-                .intercept(new MicrometerServerInterceptor(Metrics.globalRegistry, configure))
+//                .intercept(new MicrometerServerInterceptor(Metrics.globalRegistry, configure))
+                .intercept(interceptor)
                 .executor(executor);
     }
 }
