a8d5e5856b7382fd198ffca9e568af88519990cb
Joseph Yang <before30@gmail.com>
2020-03-03 23:23:04 +0900

    Add demo app

---
+++ demo-app/src/main/java/cc/before30/metricex/demo/DemoApplication.java
@@ -0,0 +1,19 @@
+package cc.before30.metricex.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * DemoApplication
+ *
+ * @author before30
+ * @since 2020/03/03
+ */
+
+@SpringBootApplication
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+}
